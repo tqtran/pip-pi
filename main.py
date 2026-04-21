e41ae18527b57c9630d45640db7d49679649e653
toan_tran_cccd <ttran16@cccd.edu>
2026-04-21 00:16:50 -0700

    Refactor main.py to remove unused code and improve initialization logging

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
+import os
 import pygame
 
+print("SDL_VIDEODRIVER =", os.environ.get("SDL_VIDEODRIVER"))
 print("before init")
 pygame.init()
 print("after init")
-
 print("display init?", pygame.display.get_init())
-
+print("about to set_mode")
 screen = pygame.display.set_mode((480, 320))
+print("driver =", pygame.display.get_driver())
 print("set_mode worked")
-
-running = True
-while running:
-    for event in pygame.event.get():
-        print(event)
-        if event.type == pygame.QUIT:
-            running = False
-
-pygame.quit()
